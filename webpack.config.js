67f5b84bc60b59ec3e4ff90be0bfa48fd7a59d94
Vipertech (Affolter Matias) <74778466+vipertechofficial@users.noreply.github.com>
2026-03-24 23:00:17 +0100

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -21,16 +21,19 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules\/(?!@pixagram)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         "presets": [
                             ["@babel/preset-env", {
                                 "targets": {
-                                    "chrome": "86"
+                                    "chrome": "100"
                                 }
                             }]
+                        ],
+                        "plugins": [
+                            "@babel/plugin-transform-optional-chaining"
                         ]
                     }
                 }
